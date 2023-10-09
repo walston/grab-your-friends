48ce6d8d2a866c0b2b72c40f1433dc261c15ccbd
Nathan Walston <walston@users.noreply.github.com>
2023-10-09 08:50:09 -0700

    everything works except the profile picture

--- src/background.ts
+++ src/background.ts
@@ -7,19 +7,29 @@ chrome.runtime.onMessage.addListener(
     const { type, ...user } = message;
     if (type !== "user") return false;
 
-    const canvas = new OffscreenCanvas(600, 100);
+    const canvas = new OffscreenCanvas(600, 105);
     const context = canvas.getContext("2d");
 
-    context.fillStyle = "black";
-    context.fillRect(-5, -5, 605, 105);
+    const bioLines = splitLines(context, user.bio);
+    canvas.height = 60 + bioLines.length * 20 + 5;
 
-    context.font = "15px PT Sans, 15px Apple Color Emoji";
+    context.fillStyle = "black";
+    context.fillRect(-5, -5, canvas.width + 10, canvas.height + 10);
+
+    // context.drawImage() // still need to grab the picture
+
+    context.font = "15px PT Sans";
     context.fillStyle = "white";
-    context.fillText(user.displayName, 64, 20, 524);
+    context.fillText(user.displayName, 64, 20);
     context.fillStyle = "rgb(113, 118, 123)";
-    context.fillText(user.username, 64, 40, 524);
+    context.fillText(user.username, 64, 40);
     context.fillStyle = "white";
-    context.fillText(user.bio, 64, 60, 524);
+
+    let y = 60;
+    for (const line of bioLines) {
+      context.fillText(line, 64, y);
+      y += 20;
+    }
 
     canvas
       .convertToBlob({ type: "image/png" })
@@ -46,4 +56,39 @@ function blobToDataUrl(blob: Blob): Promise<string> {
     file.addEventListener("error", reject);
     file.readAsDataURL(blob);
   });
+}
+
+function splitLines(
+  context: OffscreenCanvasRenderingContext2D,
+  text: string
+): string[] {
+  context.font = "15px PT Sans";
+
+  const MAX_WIDTH = 524;
+  const lines = [text];
+  while (MAX_WIDTH < context.measureText(lines.at(-1)).width) {
+    const line = lines.pop();
+    const splitIndices: number[] = [];
+    for (let i = 0; i < line.length; i++) {
+      const char = line[i];
+      if (char.match(/\s/)) splitIndices.push(i);
+      // latin unicode codeblock
+      if (char.match(/[\u0000-\u04ff]/)) continue;
+      // CJK unicode code block
+      const cjk =
+        /[\u4E00-\u9FFF\u3400-\u4DBF\u20000-\u2A6DF\u2A700-\u2B73F\u2B740-\u2B81F\u2B820-\u2CEAF\u2CEB0-\u2EBEF\u30000-\u3134F\u31350-\u323AF\uF900-\uFAFF\u2F800-\u2FA1F]/;
+      if (char.match(cjk)) splitIndices.push(i);
+    }
+
+    let i = 0;
+    while (true) {
+      let j = splitIndices.shift();
+      if (MAX_WIDTH > context.measureText(line.slice(0, j)).width) i = j;
+      else break;
+    }
+    lines.push(line.slice(0, i));
+    lines.push(line.slice(i).trimStart());
+  }
+
+  return lines;
 }
